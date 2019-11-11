6d917a5324a6710d2ab4050f52eaafb63b36500a
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-11-11 00:23:43 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    Student.create(name: params[:student][:name])
+  redirect_to posts_path
   end
 
 end
